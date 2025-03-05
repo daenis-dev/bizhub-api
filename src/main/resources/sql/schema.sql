a411bfbcb46bdbfc0d354ffbca299fa7ed9212ae
daenis-dev <daenis.dev@gmail.com>
2025-03-04 14:07:13 -1000

    Updated schema

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -43,3 +43,30 @@ CREATE TABLE schedule_keys (
 );
 
 INSERT INTO schedule_keys (user_id, token, is_active, created_date_time_in_utc) VALUES ('456-def', 'xyz', true, NOW());
+
+CREATE SEQUENCE IF NOT EXISTS booking_request_statuses_id_seq;
+CREATE TABLE booking_request_statuses (
+    id INT NOT NULL DEFAULT nextval('booking_request_statuses_id_seq') PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    request_is_pending_approval BOOLEAN NOT NULL DEFAULT TRUE,
+    request_is_active BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO booking_request_statuses (name, request_is_pending_approval) VALUES ('pending approval', TRUE);
+INSERT INTO booking_request_statuses (name, request_is_pending_approval) VALUES ('accepted', FALSE);
+INSERT INTO booking_request_statuses (name, request_is_pending_approval) VALUES ('denied', FALSE);
+INSERT INTO booking_request_statuses (name, request_is_pending_approval) VALUES ('canceled', FALSE);
+
+CREATE SEQUENCE IF NOT EXISTS booking_requests_id_seq;
+CREATE TABLE booking_requests (
+	id INT NOT NULL DEFAULT nextval('booking_requests_id_seq') PRIMARY KEY,
+	requestee_user_id VARCHAR(255) NOT NULL,
+    requester_email_address VARCHAR (255) NOT NULL,
+    status_id INT NOT NULL,
+    start_date_time_in_utc TIMESTAMPTZ NOT NULL,
+    end_date_time_in_utc TIMESTAMPTZ NOT NULL,
+    event_name VARCHAR(255) NOT NULL,
+    created_date_time_in_utc TIMESTAMPTZ NOT NULL,
+    modified_date_time_in_utc TIMESTAMPTZ NOT NULL,
+    FOREIGN KEY (status_id) REFERENCES booking_request_statuses (id)
+);
